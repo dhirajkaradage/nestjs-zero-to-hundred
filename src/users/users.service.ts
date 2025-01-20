fc214f5bbacb69b197f39c54b1e778f8040959c5
dhirajdk <dhiraj.karadage@amelioratesolutions.com>
2025-01-20 23:44:13 +0530

    added new module named address

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,5 +1,8 @@
 import { HttpException, Injectable } from '@nestjs/common';
 import { UserDTO } from './userDto/user-dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class UsersService {
@@ -17,12 +20,19 @@ export class UsersService {
       password: 'k10sutar',
     },
   ];
-  async getAllUsers(): Promise<UserDTO[]> {
-    return this.users;
+
+  constructor(
+    @InjectRepository(User) private userRepository: Repository<User>,
+  ) {}
+
+  getAllUsers(): Promise<UserDTO[]> {
+    return this.userRepository.find();
   }
 
   async getUserById(id: number): Promise<UserDTO> {
-    const user = this.users.find((obj) => obj.id === +id);
+    // const user = this.users.find((obj) => obj.id === +id);
+    const user = await this.userRepository.findOne({ where: { id: id } });
+    console.log('thsi is user ', user);
     if (!user) {
       throw new HttpException('No user found', 412);
     }
@@ -30,25 +40,30 @@ export class UsersService {
   }
 
   async createUser(userDTO: UserDTO): Promise<UserDTO> {
-    const newId = this.users[this.users.length - 1].id;
-    userDTO['id'] = newId + 1;
-    this.users.push(userDTO);
-    return this.users[this.users.length - 1];
+    // const newId = this.users[this.users.length - 1].id;
+    // userDTO['id'] = newId + 1;
+    // this.users.push(userDTO);
+    // return this.users[this.users.length - 1];
+    return await this.userRepository.save(userDTO);
   }
 
   async updateUser(id: number, userDto: UserDTO) {
-    const dbUser = this.users.find((obj) => obj.id === id);
+    // const dbUser = this.users.find((obj) => obj.id === id);
+    const dbUser = await this.userRepository.findOne({ where: { id: id } });
     if (!dbUser) throw new HttpException('No User Found', 412);
     dbUser['email'] = userDto.email;
     dbUser['name'] = userDto.name;
     dbUser['password'] = userDto.password;
-    return this.users;
+
+    const updatedUser = await this.userRepository.save(dbUser);
+    return updatedUser;
   }
 
   async deleteUser(id: number) {
-    const userToDeleteIndex = this.users.findIndex((obj) => obj.id === id);
-    if (userToDeleteIndex == -1) throw new HttpException('User no found', 412);
-    this.users.splice(userToDeleteIndex, 1);
-    return this.users;
+    // const userToDeleteIndex = this.users.findIndex((obj) => obj.id === id);
+    const userToDeleteIndex = await this.userRepository.delete(id);
+    // if (userToDeleteIndex == -1) throw new HttpException('User no found', 412);
+    // this.users.splice(userToDeleteIndex, 1);
+    return userToDeleteIndex;
   }
 }
