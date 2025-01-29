e21590e2b522ec570a150820619d8c9698c8a93d
dhirajdk <dhiraj.karadage@amelioratesolutions.com>
2025-01-29 23:30:22 +0530

    jwt token generated with username and password(normal string)

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { SignInDto } from './dto/sign-in.dto';
 
@@ -6,10 +6,13 @@ import { SignInDto } from './dto/sign-in.dto';
 export class AuthController {
   constructor(private authService: AuthService) {}
 
+  @Post('login')
   async signIn(@Body() signInDto: SignInDto) {
     const user = await this.authService.validateUser(signInDto);
+    console.log('this is user ', user);
 
     if (user) {
+      return await this.authService.login(user);
     }
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,12 +3,16 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { User } from 'src/users/user.entity';
 import { Repository } from 'typeorm';
 import { SignInDto } from './dto/sign-in.dto';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(@InjectRepository(User) private userRepo: Repository<User>) {}
+  constructor(
+    @InjectRepository(User) private userRepo: Repository<User>,
+    private jwtService: JwtService,
+  ) {}
 
-  async validateUser(signInDto: SignInDto): Promise<User> {
+  async validateUser(signInDto: SignInDto): Promise<any> {
     const user = await this.userRepo.findOne({
       where: { name: signInDto.username, password: signInDto.password },
     });
@@ -16,6 +20,16 @@ export class AuthService {
     if (!user) {
       throw new HttpException('User not found', 412);
     }
-    return user;
+
+    const { password, ...result } = user;
+    return result;
+  }
+
+  async login(user: any) {
+    const payload = { username: user.name, sub: user.id, email: user.email };
+    return {
+      ...user,
+      access_token: this.jwtService.sign(payload),
+    };
   }
 }
